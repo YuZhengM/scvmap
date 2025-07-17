8948bb6b53f44553959cdee6ec38372405a00b4b
yuzhengmin <3236170161@qq.com>
2025-07-17 15:43:02 +0800

    feat: Improve documentation

--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 @echo off
-start cmd /c "scp -r D:\output\SCVdb\API\* root@bio.liclab.net:${project_path}/data/API/"
-start cmd /c "scp -r D:\project\scvdb\target\SCVdb-1.0.0.war root@bio.liclab.net:${project_path}/data/code/service/scvdb_service.war"
+start cmd /c "scp -r D:\output\SCVdb\API\* ${username}@${IP}:${project_path}/data/API/"
+start cmd /c "scp -r D:\project\scvdb\target\SCVdb-1.0.0.war ${username}@${IP}:${project_path}/data/code/service/scvdb_service.war"
