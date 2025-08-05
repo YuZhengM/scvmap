057ad1d803e92559e5c2533a09673f8aef5bd841
yuzhengmin <3236170161@qq.com>
2025-08-05 18:15:28 +0800

    feat: Improve upload

--- upload.bat
+++ upload.bat
@@ -1,3 +1,2 @@
 @echo off
-start cmd /c "scp -r D:\output\SCVdb\API\* ${username}@${IP}:${project_path}/data/API/"
 start cmd /c "scp -r D:\project\scvdb\target\SCVdb-1.0.0.war ${username}@${IP}:${project_path}/data/code/service/scvdb_service.war"
